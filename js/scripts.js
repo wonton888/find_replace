1f5026239cb17a619af2b0fe22ae9050ab3baa1c
Adam <wonton916@gmail.com>
2015-09-11 17:57:42 -0700

    add several tests to specs.js, all tests are passing. started code review late today

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//used Regex statement
+//used Regex statement, ignoring case type for user input
 
 var find_replace = function(string, findWord, replace){
   return string.toLowerCase().replace(new RegExp("(" +findWord+ ")", 'gi'), replace);
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,20 @@
 describe("findReplace", function(){
-  it ("returns a single word inputed", function(){
-    expect(find_replace("Apple", "apple", "orange")).to.equal("orange");
+  it ("returns a single word inputed with replacement word", function(){
+    expect(find_replace("apple", "apple", "orange")).to.equal("orange");
+  });
+  it ("returns a string of words with replacement word", function(){
+    expect(find_replace("hello world", "world", "bed")).to.equal("hello bed");
+  });
+  it ("returns a single word inputed when user replacement word is not found in user input", function(){
+    expect(find_replace("apple", "kiwi", "grapes")).to.equal("apple");
+  });
+  it ("returns a string of words when user replacement word is not found in user input", function(){
+    expect(find_replace("hello world", "howdy", "orange")).to.equal("hello world");
+  });
+  it ("returns a string of words with two instances of the replacement word", function(){
+    expect(find_replace("hello world world", "world", "food")).to.equal("hello food food");
+  });
+  it ("returns a single capitalized word inputed with replacement word", function(){
+    expect(find_replace("Hello teacher", "Hello", "Bad")).to.equal("Bad teacher");
   });
 });
